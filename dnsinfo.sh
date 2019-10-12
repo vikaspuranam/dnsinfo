7a433f5ad4cc67d251e5471b878fd7d5a2bc477a
vikaspuranam <56467772+vikaspuranam@users.noreply.github.com>
2019-10-12 12:20:31 +0530

    Update dnsinfo.sh

--- dnsinfo.sh
+++ dnsinfo.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#Take Input
-#printf "Please Enter Domain Name: "
-#read domain
-#DNS Resolve
-#printf "Host Name Is : \n"
-#host -t a $domain|grep "has address"|cut -d " " -f 4
-#NS Extraction
-#printf "Nameservers Are : \n"
-#host -t ns $domain|grep "name server"|cut -d " " -f 4 > /tmp/ns.txt
-#cat /tmp/ns.txt | while read line; do host -t a $line; done|grep "has address"|cut -d " " -f 4
-#MS Extraction
-#printf "Mail Exchange Servers Are : \n"
-#host -t mx hackerschool.in|grep "is handled"|cut -d " " -f 7 > /tmp/mx.txt
-#cat /tmp/mx.txt | while read line; do host -t a $line; done|grep "has address"|cut -d " " -f 4
-
-
 
 #Take Input
 printf "Please Enter Domain Name: "
@@ -39,10 +23,3 @@ printf '\n'
 printf "Mail Exchange Servers Are : \n\n"
 host -t mx hackerschool.in|grep "is handled"|cut -d " " -f 7 > /tmp/mx.txt
 cat /tmp/mx.txt | while read line; do printf $line'%-10s%-14s';host -t a $line|grep "has address"|cut -$
-
-
-
-
-
-
-#CHINNI
